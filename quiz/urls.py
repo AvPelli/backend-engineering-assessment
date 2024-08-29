65dfc7fa8827c6db20aeb548e67cbff32695d735
AvPelli <arthur.van.pellicom@hotmail.com>
2024-08-29 18:12:21 +0200

    resolve /admin url conflict, include quiz.urls

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,12 +4,11 @@ from rest_framework.routers import DefaultRouter
 from .views import QuizViewSet, QuestionViewSet, AnswerViewSet, ParticipantViewSet
 
 router = DefaultRouter()
-router.register(r'quizzes', QuizViewSet)
+router.register(r'quiz', QuizViewSet)
 router.register(r'questions', QuestionViewSet)
 router.register(r'answers', AnswerViewSet)
 router.register(r'participants', ParticipantViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
