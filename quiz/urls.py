0613744eef726b0993f32ed78546a550683b6794
AvPelli <arthur.van.pellicom@hotmail.com>
2024-08-29 19:28:06 +0200

    Simple quiz frontend accessible by quiz/{id}

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuizViewSet, QuestionViewSet, AnswerViewSet, ParticipantViewSet
+from .views import QuizViewSet, QuestionViewSet, AnswerViewSet, ParticipantViewSet, quiz_view
 
 router = DefaultRouter()
 router.register(r'quiz', QuizViewSet)
@@ -11,4 +11,6 @@ router.register(r'participants', ParticipantViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('quiz/<int:id>/',quiz_view, name='quiz'),
+    path('api/quiz/<int:pk>/questions/', QuizViewSet.as_view({'get': 'questions'}), name='quiz-questions'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,13 +1,27 @@
-#from django.shortcuts import render
-
+from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Quiz, Question, Answer, Participant
 from .serializers import QuizSerializer, QuestionSerializer, AnswerSerializer, ParticipantSerializer
+
+
+def quiz_view(request, id):
+    return render(request, 'quiz.html', {'quiz_id': id})
+
 
 #Return data in JSON format by serializing objects -> these can be answers to API calls
 class QuizViewSet(viewsets.ModelViewSet):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
+
+    @action(detail=True, methods=['get'])
+    def questions(self, request, pk=None):
+        quiz = self.get_object()
+        questions = Question.objects.filter(quiz=quiz)
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
