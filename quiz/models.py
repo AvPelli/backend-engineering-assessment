ab5f3a670113da6b51b22774b48c9d61a664795f
AvPelli <arthur.van.pellicom@hotmail.com>
2024-08-29 18:11:11 +0200

    Appropriate db names for quiz models

--- quiz/models.py
+++ quiz/models.py
@@ -8,12 +8,19 @@ class Quiz(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        db_table='quiz'
+
 class Question(models.Model):
+    #cascading delete, ensuring all questions are deleted when quiz is deleted
     quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
 
     def __str__(self):
         return self.text
+
+    class Meta:
+        db_table='question'
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
@@ -23,6 +30,9 @@ class Answer(models.Model):
     def __str__(self):
         return self.text
 
+    class Meta:
+        db_table='answer'
+
 class Participant(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
@@ -30,4 +40,7 @@ class Participant(models.Model):
 
     def __str__(self):
         return f'{self.user.username} - {self.quiz.title}'
+
+    class Meta:
+        db_table='participant'
 
